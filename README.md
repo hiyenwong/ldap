01a178ff2f61590b8887b65ae71ff31eee042d8a
HiYen Wong <robinwong51@qq.com>
2019-10-05 16:00:17 +0800

    修改LDAP用户登入

--- README.md
+++ README.md
@@ -16,3 +16,12 @@ AD, LDAP 的认证， 从mantis 的LDAP code 扩展剪裁。在win2003+active di
 
 - admin 系统管理员跳过LDAP 认证，如果你系统超级管理员不叫 admin,自行修改 `module/user/ext/model/ldap.php` 文件第8行。
 - 修复 `lib/ldap/ldap.class.php` php Warnning
+
+
+修改/model/user/js/login.js 过滤admin这个账号
+```javascript
+          if ($('#account').val() == 'admin'){
+                  if(password.length != 32 && typeof(md5) == 'function') $('input:password').val(md5(md5(password) + rand)); 36         }else{
+                  if(password.length != 32 && typeof(md5) == 'function') $('input:password').val(password);
+          }
+```
--- module/user/ext/model/ldap.php
+++ module/user/ext/model/ldap.php
@@ -1,3 +1,4 @@
+<?php
 /* vim: set expandtab sw=4 ts=4 sts=4: */
 
 function identify($account, $password)
@@ -7,10 +8,10 @@ function identify($account, $password)
     /* admin account skip  LDAP  Authenticate */
     if ($account == "admin") {
         $record = $this->dao->select('*')->from(TABLE_USER)
-                        ->where('account')->eq($account)
-                        ->beginIF(strlen($password) < 32)->andWhere('password')->eq(md5($password))->fi()
-                        ->andWhere('deleted')->eq(0)
-                        ->fetch();
+            ->where('account')->eq($account)
+            ->beginIF(strlen($password) < 32)->andWhere('password')->eq(md5($password))->fi()
+            ->andWhere('deleted')->eq(0)
+            ->fetch();
 
         /* If the length of $password is 32 or 40, checking by the auth hash. */
         $user = false;
@@ -27,7 +28,7 @@ function identify($account, $password)
                 $user = $password == $hash ? $record : '';
             }
 
-            $ip   = $this->server->remote_addr;
+            $ip = $this->server->remote_addr;
             $last = $this->server->request_time;
             $this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($account)->exec();
             $user->last = date(DT_DATETIME1, $user->last);
@@ -38,22 +39,46 @@ function identify($account, $password)
 
     /* Authenticate with LDAP */
     $this->app->loadClass('ldap', true);
-    if (!ldap_authenticate_by_username($account, $password)) {
+    $ldap_user = ldap_authenticate_by_username($account, $password);
+    if (!$ldap_user) {
         return false;
     }
 
     /* Get the user from database without check password condition.  */
     $user = $this->dao->select('*')->from(TABLE_USER)
-                    ->where('account')->eq($account)
-                    ->andWhere('deleted')->eq(0)
-                    ->fetch();
+        ->where('account')->eq($account)
+        ->andWhere('deleted')->eq(0)
+        ->fetch();
 
 
     if ($user) {
-        $ip   = $this->server->remote_addr;
+        $ip = $this->server->remote_addr;
         $last = $this->server->request_time;
         $this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($account)->exec();
         $user->last = date(DT_DATETIME1, $user->last);
+    } else {
+        $deletedUser = $this->dao->select('*')->from(TABLE_USER)
+            ->where('account')->eq($account)
+            ->andWhere('deleted')->eq(1)
+            ->fetch();
+
+        if ($deletedUser) {
+            return false;
+        }
+        //如果没有用户则创建用户
+        $user_info = new stdClass();
+        $user_info->account = $account;
+        $user_info->email = $account . '@iresearch.com.cn';
+        $user_info->password = md5($password);
+        $user_info->gender = null;
+        $user_info->realname = $ldap_user[0]['displayname'][0];
+        $user = $this->dao->insert(TABLE_USER)->data($user_info)->autoCheck()->exec();
+        //默认添加组
+        $group = new stdClass();
+        $group->account = $user->account;
+        $group->group = 11;
+        $this->dao->replace(TABLE_USERGROUP)->data($group)->exec();
+        $user = true;
     }
 
     return $user;
